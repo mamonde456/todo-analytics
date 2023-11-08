b3e1e3ee0caddef37378099af5906f10ddb57946
mamonde456 <mamonde456@gmail.com>
2023-11-08 13:31:52 +0900

    feat: date picker 전역 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,13 @@ import App from "./App.vue";
 import router from "./router";
 import store, { key } from "./store/index";
 
+import VueDatePicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
+
 const app = createApp(App);
 
 app.use(store, key);
 app.use(router);
+app.component("VueDatePicker", VueDatePicker);
 
 app.mount("#app");
