16386f9f2cbe1234ad4ed49c7e1bd514f3c19062
mamonde456 <mamonde456@gmail.com>
2023-11-08 13:32:32 +0900

    feat: vuex에 더미 데이터 추가 및 타입 수정

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,20 +1,58 @@
 import { createLogger, createStore, Store } from "vuex";
 import type { InjectionKey } from "vue";
 
-// define your typings for the store state
+export type Category = "제목" | "설명" | "상태" | "날짜";
 export interface State {
-  category: string;
+  category: Category;
 }
 
-// define injection key
 export const key: InjectionKey<Store<State>> = Symbol();
 
 export default createStore({
   state: {
     category: "제목",
+    dummyData: {
+      todoList: [
+        {
+          id: "1",
+          title: "커피 사러 가기",
+          content: "점심시간에 커피 사러 가기",
+          date: ["2023-11-08T12:00:00.000Z"],
+          status: "진행전",
+        },
+        {
+          id: "2",
+          title: "프로젝트 회의",
+          content: "오후 2시에 온라인으로 프로젝트 회의 참석하기",
+          date: ["2023-11-08T14:00:00.000Z"],
+          status: "진행중",
+        },
+        {
+          id: "3",
+          title: "운동하기",
+          content: "퇴근 후 30분 동안 집에서 운동하기",
+          date: ["2023-11-08T18:00:00.000Z"],
+          status: "진행전",
+        },
+        {
+          id: "4",
+          title: "저녁 약속",
+          content: "친구와 저녁 약속, 7시에 만나기",
+          date: ["2023-11-08T19:00:00.000Z"],
+          status: "진행전",
+        },
+        {
+          id: "5",
+          title: "책 읽기",
+          content: "자기 전에 책 읽기 30분",
+          date: ["2023-11-08T22:00:00.000Z"],
+          status: "완료",
+        },
+      ],
+    },
   },
   mutations: {
-    setCategory(state: State, category: string) {
+    SET_CATEGORY(state: State, category: Category) {
       state.category = category;
     },
   },
