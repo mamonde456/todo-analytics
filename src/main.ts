55f07780df2436b1aec18ea54f66c7092b603ec0
mamonde456 <mamonde456@gmail.com>
2023-11-10 14:35:14 +0900

    feat: vue apex chart 설정

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,13 @@ import store, { key } from "./store/index";
 import VueDatePicker from "@vuepic/vue-datepicker";
 import "@vuepic/vue-datepicker/dist/main.css";
 
+import VueApexCharts from "vue3-apexcharts";
+
 const app = createApp(App);
 
 app.use(store, key);
 app.use(router);
+app.use(VueApexCharts);
 app.component("VueDatePicker", VueDatePicker);
 
 app.mount("#app");
